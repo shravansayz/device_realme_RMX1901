21e233461e88513a654d2a70894d0a3d2a5e56f2
Kelvin Zhang <zhangkelvin@google.com>
2024-09-08 13:32:28 +0530

    RMX1901: Allow to access bootable/deprecated-ota targets
    r5x is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to r5x namespace list.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- device.mk
+++ device.mk
@@ -496,6 +496,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
