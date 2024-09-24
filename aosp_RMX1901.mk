dbc4e2785321c8071a20733a3cff7bd6ffa897ae
Shravan Sayz <shravankumarsayz@gmail.com>
2024-09-24 13:23:55 +0000

    RMX1901: Drop ViPER4Android FX support
    Signed-off-by: Shravan Sayz <shravankumarsayz@gmail.com>

--- aosp_RMX1901.mk
+++ aosp_RMX1901.mk
@@ -16,13 +16,8 @@ TARGET_GAPPS_ARCH := arm64
 
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
