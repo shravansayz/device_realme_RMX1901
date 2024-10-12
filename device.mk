de3ca387901007c8764c488d9d64d838cf35edc2
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    RMX1901: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- device.mk
+++ device.mk
@@ -506,6 +506,8 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     android.media.audio.common.types-V2-cpp \
+    libgui_shim \
+    libinput_shim \
     libnl \
     libpng.vendor \
     libprocessgroup.vendor \
