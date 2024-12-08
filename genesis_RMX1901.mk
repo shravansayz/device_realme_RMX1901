0f0b770dda097c191906ff2e23d8cf107659c4c9
Shravan Sayz <shravankumarsayz@gmail.com>
2024-12-08 10:05:29 +0000

    RMX1901: Bringup Gen
    Signed-off-by: Shravan Sayz <shravankumarsayz@gmail.com>

--- genesis_RMX1901.mk
+++ genesis_RMX1901.mk
@@ -9,12 +9,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-WITH_GAPPS := true
 TARGET_DISABLE_EPPE := true
-TARGET_GAPPS_ARCH := arm64
 
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +19,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := aosp_RMX1901
+PRODUCT_NAME := genesis_RMX1901
 PRODUCT_MODEL := RMX1901
 
 
